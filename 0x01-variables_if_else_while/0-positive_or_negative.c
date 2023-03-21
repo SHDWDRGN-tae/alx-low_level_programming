7d73ee4a267993436c4763f8d77199f10b780950
SHDWDRGN <taepeolwane@gmail.com>
2023-03-20 20:35:16 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
 /**
  * main - n main function
  *
- *Return: always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -18,15 +18,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
