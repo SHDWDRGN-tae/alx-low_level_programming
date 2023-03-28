f40c61ba4faa104908cd231c8fc5c440ca6f0863
SHDWDRGN <taepeolwane@gmail.com>
2023-03-27 19:44:27 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - replace current value with 98
+  *
+  * @n: is the parametter to be reset
+  *
+  * Return: always 0
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
