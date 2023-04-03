c79ff67856bd56170f82e394bc443ad6ec14a81b
SHDWDRGN <taepeolwane@gmail.com>
2023-04-02 19:59:08 -0700

    6. There is no such thing as absolute value in this world

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
-/**
-  * print_alphabet_10x - print alphabet 10x in lowercase
-  *
-  * Return: always 0;
-  */
-void print_alphabet_10x(void)
-{
-	int i;
-	char a;
 
-	for  (i = 1 ; i <= 10 ; i++)
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+	void print_alphabet_x10(void)
 	{
-		for (a = 'a' ; a <= 'z' ; a++)
-			_putchar(a);
-		_putchar('\n');
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
-}
